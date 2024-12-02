f8c40ee57361bde1624c2f9cad26a2fdfbec493d
Lennart Kasserra <121236725+kssrr@users.noreply.github.com>
2024-12-02 18:07:48 +0100

    aufräumen

--- 01/solution_p1.py
+++
@@ -1,8 +0,0 @@
-with open("input", "r") as infile:
-    left, right = zip(*[map(int, line.split()) for line in infile])
-
-left = sorted(left)
-right = sorted(right)
-
-sum([abs(l - r) for l, r in zip(left, right)])
-
--- 01/solution_p2.py
+++
@@ -1,5 +0,0 @@
-with open("input", "r") as infile:
-    left, right = zip(*[map(int, line.split()) for line in infile])
-
-diff = sum([x * sum([1 for y in right if y == x]) for x in left])
-print(f"Total difference: {diff}")
